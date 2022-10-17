fc42741e7913bd34d58fb01714f66d179eae8a1c
Yaroslav Baiduk <byaa99@gmail.com>
2022-10-17 14:15:13 +0300

    Fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,6 @@ export default defineNuxtConfig({
   ssr: false,
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   router: {
-    base: '/weather-app'
+    base: 'https://ant2nta.github.io/weather-app/'
   }
 })
